67817d2f8f764e8fc971b2d427fd762a6834c9fc
Abhignan Rakshith <abhignanrakshith@outlook.com>
2024-01-05 22:58:32 +0530

    Adding Side_Projects

---
+++ Side_Projects/invertString.py
@@ -0,0 +1,16 @@
+# Ways to invert a string
+# my_string = "Hello World!"
+
+# myList = [*my_string]
+# myList = myList[::-1]
+# word = ""
+# for letter in myList:
+#     word+=letter
+# print(word)
+# for i in range(len(myList)):
+#     print("%c"%myList[i], end="")
+    
+#Or
+my_string = "Hello World!"
+my_string = my_string[::-1]
+print(my_string)
